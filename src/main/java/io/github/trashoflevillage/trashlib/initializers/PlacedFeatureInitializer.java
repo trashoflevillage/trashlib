aea7f7489c483352b7e3311317a2564b8a511991
trashoflevillage <uhhnokthxbye@gmail.com>
2025-02-20 14:45:03 -0500

    Added ConfiguredFeatureInitializer and PlacedFeatureInitializer

---
+++ src/main/java/io/github/trashoflevillage/trashlib/initializers/PlacedFeatureInitializer.java
@@ -0,0 +1,16 @@
+package io.github.trashoflevillage.trashlib.initializers;
+
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.feature.PlacedFeature;
+
+public class PlacedFeatureInitializer extends AbstractInitializer {
+    public PlacedFeatureInitializer(String modId) {
+        super(modId);
+    }
+
+    public RegistryKey<PlacedFeature> register(String name) {
+        return RegistryKey.of(RegistryKeys.PLACED_FEATURE, Identifier.of(MOD_ID, name));
+    }
+}
