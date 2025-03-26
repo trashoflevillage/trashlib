b26c42d5d1012b62c293b6ba759ba724b82b426b
trashoflevillage <uhhnokthxbye@gmail.com>
2025-03-26 10:33:29 -0400

    Added ConventionalTags stuff

--- common/src/main/java/io/github/trashoflevillage/trashlib/util/ConventionalTags.java
+++ common/src/main/java/io/github/trashoflevillage/trashlib/util/ConventionalTags.java
@@ -1,6 +1,8 @@
 package io.github.trashoflevillage.trashlib.util;
 
 import io.github.trashoflevillage.trashlib.initializers.TagInitializer;
+import net.minecraft.block.Block;
+import net.minecraft.entity.EntityType;
 import net.minecraft.item.Item;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.TagKey;
@@ -9,9 +11,31 @@ public class ConventionalTags {
     public static class Items {
         private static final TagInitializer<Item> initializer = new TagInitializer<>("c", RegistryKeys.ITEM);
 
-        public static final TagKey<Item> CONCRETE_POWDERS = initializer.register("concrete_powders");
-        public static final TagKey<Item> CONCRETE = initializer.register("concrete");
-        public static final TagKey<Item> GLASS_BLOCKS = initializer.register("glass_blocks");
-        public static final TagKey<Item> GLASS_PANES = initializer.register("glass_panes");
+        public static final TagKey<Item> CONCRETE_POWDERS = of("concrete_powders");
+        public static final TagKey<Item> CONCRETE = of("concrete");
+        public static final TagKey<Item> GLASS_BLOCKS = of("glass_blocks");
+        public static final TagKey<Item> GLASS_PANES = of("glass_panes");
+        public static final TagKey<Item> SHULKER_BOXES = of("shulker_boxes");
+        public static final TagKey<Item> GLAZED_TERRACOTTAS = of("glazed_terracottas");
+        
+        public static TagKey<Item> of(String name) {
+            return initializer.register(name);
+        }
+    }
+
+    public static class Blocks {
+        private static final TagInitializer<Block> initializer = new TagInitializer<>("c", RegistryKeys.BLOCK);
+
+        public static TagKey<Block> of(String name) {
+            return initializer.register(name);
+        }
+    }
+
+    public static class EntityTypes {
+        private static final TagInitializer<EntityType<?>> initializer = new TagInitializer<>("c", RegistryKeys.ENTITY_TYPE);
+
+        public static TagKey<EntityType<?>> of(String name) {
+            return initializer.register(name);
+        }
     }
 }
